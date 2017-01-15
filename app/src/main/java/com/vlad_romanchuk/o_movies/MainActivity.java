e3d34f9f841d8c6691ccb94522fc871cc9e7578c
vladrom <wadoz.1995@gmail.com>
2017-01-15 18:22:09 +0200

    adapter finish

--- app/src/main/java/com/vlad_romanchuk/o_movies/MainActivity.java
+++ app/src/main/java/com/vlad_romanchuk/o_movies/MainActivity.java
@@ -1,14 +1,20 @@
 package com.vlad_romanchuk.o_movies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/vlad_romanchuk/o_movies/films/MovieAdapter.java
+++ app/src/main/java/com/vlad_romanchuk/o_movies/films/MovieAdapter.java
@@ -1,6 +1,11 @@
 package com.vlad_romanchuk.o_movies.films;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.vlad_romanchuk.o_movies.R;
 import com.vlad_romanchuk.o_movies.model.Movie;
 
@@ -15,13 +22,13 @@ import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
 
-    public List<Movie> movies;
+    public List<Movie> movieList;
 
     private LayoutInflater inflater;
     private Contract.Presenter presenter;
 
-    public MovieAdapter(List<Movie> movies, Contract.Presenter presenter) {
-        this.movies = movies;
+    public MovieAdapter(List<Movie> movieList, Contract.Presenter presenter) {
+        this.movieList = movieList;
         this.presenter = presenter;
     }
 
@@ -34,12 +41,39 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder>
 
     @Override
     public void onBindViewHolder(MovieHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+
+        final Movie movie = movieList.get(position);
+
+        holder.nameFilm.setText(movie.getTitle());
+        holder.rating.setText(movie.getVoteAverage().toString());
+
+        Glide
+                .with(context)
+                .load("http://image.tmdb.org/t/p/w185/" + movie.getPosterPath())
+                .asBitmap()
+                .into(new BitmapImageViewTarget(holder.pictureFilm) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(getView().getResources(), resource);
+                        drawable.setCornerRadius(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getView().getResources().getDisplayMetrics()));
+                        setDrawable(drawable);
+                    }
+                });
+
+        holder.aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.loadMovies(movie.getId() + "");
+            }
+        });
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
 
     public class MovieHolder extends RecyclerView.ViewHolder {
@@ -52,6 +86,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder>
 
         public MovieHolder(View itemView) {
             super(itemView);
+            pictureFilm = (ImageView) itemView.findViewById(R.id.image_film);
+
+            rating = (TextView) itemView.findViewById(R.id.rating_text_view);
+            nameFilm = (TextView) itemView.findViewById(R.id.film_name);
+
+            aboutButton = (Button) itemView.findViewById(R.id.about_button);
+            trailerButton = (Button) itemView.findViewById(R.id.trailer_button);
         }
     }
 }
+
